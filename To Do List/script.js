5fb34fdaa31ad87f495f1d18ccec7a7fbf4e1535
Bruno Fredes <brunofredes99@gmail.com>
2023-05-20 21:32:01 -0300

    javascript and layout update

--- To Do List/script.js
+++ To Do List/script.js
@@ -4,11 +4,13 @@ const buttons = document.querySelectorAll('.calculator button');
 buttons.forEach(button => {
   button.addEventListener('click', () => {
     const buttonText = button.textContent;
-    
+
     if (buttonText === '=') {
       evaluateExpression();
     } else if (buttonText === 'Clear') {
       clearDisplay();
+    } else if (button.classList.contains('delete')) {
+      deleteLastNumber();
     } else {
       appendToDisplay(buttonText);
     }
@@ -23,12 +25,17 @@ function clearDisplay() {
   result.value = '';
 }
 
+function deleteLastNumber() {
+  const currentValue = result.value;
+  result.value = currentValue.slice(0, -1);
+}
+
 function evaluateExpression() {
   try {
     const expression = result.value;
     const sanitizedExpression = expression.replace(/[^-()\d/*+.]/g, '');
     const resultValue = eval(sanitizedExpression);
-    
+
     if (Number.isFinite(resultValue)) {
       result.value = resultValue;
     } else {
@@ -39,3 +46,33 @@ function evaluateExpression() {
     console.error(error);
   }
 }
+
+document.addEventListener('keydown', event => {
+  const key = event.key;
+
+  if (key === 'Delete' || key === 'Backspace') {
+    const deleteButton = [...buttons].find(button => button.classList.contains('delete'));
+
+    if (deleteButton) {
+      deleteButton.click();
+    }
+  } else if (key === 'Enter') {
+    const equalButton = [...buttons].find(button => button.textContent === '=');
+
+    if (equalButton) {
+      equalButton.click();
+    }
+  } else if (key.toLowerCase() === 'c') {
+    const clearButton = [...buttons].find(button => button.textContent.toLowerCase() === 'clear');
+
+    if (clearButton) {
+      clearButton.click();
+    }
+  } else {
+    const keyButton = [...buttons].find(button => button.textContent === key);
+
+    if (keyButton) {
+      keyButton.click();
+    }
+  }
+});
